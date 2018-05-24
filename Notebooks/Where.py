fc2d9d85001c67cd00393e63356876463713d31e
Laurent Perrinet <laurent.perrinet@gmail.com>
2018-05-24 16:13:43 +0200

    minor changes / TODO : use argparse

--- Notebooks/Where.py
+++ Notebooks/Where.py
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-
-
-
-
-
 import os
 import time
 import matplotlib.pyplot as plt
@@ -14,8 +8,6 @@ import argparse
 N_theta, N_orient, N_scale, N_phase, N_X, N_Y, rho = 6, 12, 5, 2, 128, 128, 1.61803
 
 
-=======
->>>>>>> ccaaa00415f011fc67fcb79c7e3c94c4bcf2090d
 sample_size = 100  # quantity of examples that'll be processed
 N_theta, N_orient, N_scale, N_phase, N_X, N_Y, rho = 6, 12, 5, 2, 128, 128, 1.61803
 lr = 0.05
@@ -41,22 +33,12 @@ parser.add_argument('--seed', type=int, default=1, metavar='S',
 parser.add_argument('--log-interval', type=int, default=10, metavar='N',
                     help='how many batches to wait before logging training status')
 parser.add_argument('--dimension', type=int, default = 120, metavar='D',
-                    help='the dimension of the second neuron network') #ajout de l'argument dimension représentant le nombre de neurone dans la deuxième couche. 
+                    help='the dimension of the second neuron network') #ajout de l'argument dimension représentant le nombre de neurone dans la deuxième couche.
 parser.add_argument('--boucle', type=int, default=0, metavar='B',
                    help='boucle pour faire différents couche de la deuxième couche de neurone')# ajout de boucle pour automatiser le nombre de neurone dans la deuxieme couche
 args = parser.parse_args()
 
 
-<<<<<<< HEAD
-#import noise
-=======
-import os
-import time
-import matplotlib.pyplot as plt
-import numpy as np
-import argparse
-import noise
->>>>>>> ccaaa00415f011fc67fcb79c7e3c94c4bcf2090d
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -76,8 +58,8 @@ if os.path.isfile(path):
 else:
     print('No accuracy data found.')
 
-    
-## Préparer l'apprentissage et les fonctions nécessaires au fonctionnement du script 
+
+## Préparer l'apprentissage et les fonctions nécessaires au fonctionnement du script
 def vectorization(N_theta, N_orient, N_scale, N_phase, N_X, N_Y, rho):
     phi = np.zeros((N_theta, N_orient, N_scale, N_phase, N_X*N_Y))
     parameterfile = 'https://raw.githubusercontent.com/bicv/LogGabor/master/default_param.py'
@@ -229,7 +211,7 @@ class Net(torch.nn.Module):
 net = Net(n_feature=N_theta*N_orient*N_scale*N_phase, n_hidden1=n_hidden1, n_hidden2=n_hidden2, n_output=N_orient*N_scale).to(device)
 optimizer = torch.optim.SGD(net.parameters(), lr=lr)
 loss_func = torch.nn.BCEWithLogitsLoss()
-   
+
 def train(net, sample_size, optimizer=optimizer, vsize=N_theta*N_orient*N_scale*N_phase, asize=N_orient*N_scale, offset_std=10, offset_max=25, verbose=1):
     t_start = time.time()
     if verbose: print('Starting training...')
@@ -238,13 +220,13 @@ def train(net, sample_size, optimizer=optimizer, vsize=N_theta*N_orient*N_scale*
             (sample_size, 1, asize))
         target = np.zeros((sample_size, asize))
         for idx in range(sample_size):
-            i_offset, j_offset = minmax(np.random.randn()*offset_std, offset_max),  
+            i_offset, j_offset = minmax(np.random.randn()*offset_std, offset_max),
                                  minmax(np.random.randn()*offset_std, offset_max)
-            input[idx, 0, :], a_data[idx, 0, :] = couples(data[idx, 0, :], i_offset, j_offset, 
+            input[idx, 0, :], a_data[idx, 0, :] = couples(data[idx, 0, :], i_offset, j_offset,
                                                                                        device)
             target[idx, :] = a_data[idx, 0, :]
 
-        input, target = Variable(torch.FloatTensor(input)), 
+        input, target = Variable(torch.FloatTensor(input)),
                         Variable(torch.FloatTensor(a_data))
 
         prediction = net(input)
@@ -272,7 +254,7 @@ def test(net, sample_size, optimizer=optimizer, vsize=N_theta*N_orient*N_scale*N
             input[idx, 0, :], a_data[idx, 0, :] = couples(data[idx, 0, :], i_offset, j_offset, device)
             target[idx, :] = a_data[idx, 0, :]
 
-        input, target = Variable(torch.FloatTensor(input)), 
+        input, target = Variable(torch.FloatTensor(input)),
                         Variable(torch.FloatTensor(a_data))
 
         prediction = net(input)
@@ -285,7 +267,7 @@ def test(net, sample_size, optimizer=optimizer, vsize=N_theta*N_orient*N_scale*N
 def eval_sacc(vsize=N_theta*N_orient*N_scale*N_phase, asize=N_orient*N_scale, N_pic=N_X, sacc_lim=5, fovea_size=10, fig_type='cmap'):
     for batch_idx, (data, label) in enumerate(data_loader):
         data = data.to(device)
-        i_offset, j_offset = minmax(np.random.randn()*10, 35), 
+        i_offset, j_offset = minmax(np.random.randn()*10, 35),
                              minmax(np.random.randn()*10, 35)
         print('Stimulus position: ({},{})'.format(i_offset, j_offset))
         a_data_in_fovea = False
@@ -294,7 +276,7 @@ def eval_sacc(vsize=N_theta*N_orient*N_scale*N_phase, asize=N_orient*N_scale, N_
         while not a_data_in_fovea:
             input, a_data = np.zeros((1, 1, vsize)), np.zeros((1, 1, asize))
             input[0, 0, :], a_data[0, 0, :] = couples(data[0, 0, :], i_offset, j_offset, device)
-            input, a_data = Variable(torch.FloatTensor(input)), 
+            input, a_data = Variable(torch.FloatTensor(input)),
                             Variable(torch.FloatTensor(a_data))
 
             prediction = net(input)
@@ -352,7 +334,7 @@ def eval_sacc(vsize=N_theta*N_orient*N_scale*N_phase, asize=N_orient*N_scale, N_
                     theta_a_data = np.arctan(-i_offset / j_offset)
                 else:
                     theta_a_data = np.sign(-i_offset) * np.pi/2
-                print('a_data position (log_r, theta) = ({},{})'.format(log_r_a_data, 
+                print('a_data position (log_r, theta) = ({},{})'.format(log_r_a_data,
                                                                         theta_a_data))
                 log_r, theta = np.meshgrid(np.linspace(0, 1, N_scale+1), np.linspace(-np.pi*.625, np.pi*1.375, N_orient+1))
 
@@ -370,19 +352,19 @@ def eval_sacc(vsize=N_theta*N_orient*N_scale*N_phase, asize=N_orient*N_scale, N_
 
         print('*' * 50)
         return prediction
-    
-    
+
+
 
 def main():
     for epoch in range(1, args.epochs + 1):
         train(epoch)
         Accuracy = test()
     print('Test set: Final Accuracy: {:.3f}%'.format(Accuracy*100)) # print que le pourcentage de réussite final
-    
-    
-if __name__ == '__main__':  
+
+
+if __name__ == '__main__':
     if args.boucle == 1: # Pour que la boucle se fasse indiquer --boucle 1
-        rho = 10**(1/3) 
+        rho = 10**(1/3)
         for i in [int (k) for k in rho**np.arange(2,9)]:# i prend les valeur en entier du tuple rho correspondra au nombre de neurone
             args.dimension = i
             print ('La deuxième couche de neurone comporte',i,'neurones')
@@ -394,4 +376,4 @@ if __name__ == '__main__':
 
         t1 = time.time () # ajout de la constante de temps t1
 
-        print ("Le programme a mis",t1-t0, "secondes à s'exécuter.") #compare t1 et t0, connaitre le temps d'execution du programme    
+        print ("Le programme a mis",t1-t0, "secondes à s'exécuter.") #compare t1 et t0, connaitre le temps d'execution du programme
