cd27c05bf3076873129394b792dd69b23d92ba7c
pierrealbiges <pierre.albiges@etu.univ-amu.fr>
2018-05-24 15:35:06 +0200

    Bit of cleaning

--- Notebooks/Where.py
+++ Notebooks/Where.py
@@ -1,14 +1,8 @@
-
-
-N_theta, N_orient, N_scale, N_phase, N_X, N_Y, rho = 6, 12, 5, 2, 128, 128, 1.61803
-
-
 sample_size = 100  # quantity of examples that'll be processed
+N_theta, N_orient, N_scale, N_phase, N_X, N_Y, rho = 6, 12, 5, 2, 128, 128, 1.61803
 lr = 0.05
-n_hidden1 = 160
-n_RNN = 90
-n_hidden2 = 195
-
+n_hidden1 = ((N_theta*N_orient*N_scale*N_phase)/4)*3
+n_hidden2 = ((N_theta*N_orient*N_scale*N_phase)/4)
 
 
 import os
@@ -16,7 +10,7 @@ import time
 import matplotlib.pyplot as plt
 import numpy as np
 import argparse
-#import noise
+import noise
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -26,8 +20,8 @@ from torchvision import datasets, transforms
 from torch.autograd import Variable
 from LogGabor import LogGabor
 
-## Charger la matrice de certitude
 
+## Charger la matrice de certitude
 path = "MNIST_accuracy.npy"
 if os.path.isfile(path):
     print('Loading accuracy...')
@@ -36,8 +30,8 @@ if os.path.isfile(path):
 else:
     print('No accuracy data found.')
 
-## Préparer l'apprentissage et les fonctions nécessaires au fonctionnement du script
     
+## Préparer l'apprentissage et les fonctions nécessaires au fonctionnement du script 
 def vectorization(N_theta, N_orient, N_scale, N_phase, N_X, N_Y, rho):
     phi = np.zeros((N_theta, N_orient, N_scale, N_phase, N_X*N_Y))
     parameterfile = 'https://raw.githubusercontent.com/bicv/LogGabor/master/default_param.py'
@@ -45,9 +39,6 @@ def vectorization(N_theta, N_orient, N_scale, N_phase, N_X, N_Y, rho):
     lg.set_size((N_X, N_Y))
     params = {'sf_0': .1, 'B_sf': 2*lg.pe.B_sf,
               'theta': np.pi * 5 / 7., 'B_theta': 2*lg.pe.B_theta}
-
-
-
     phase = np.pi/4
     edge = lg.normalize(lg.invert(lg.loggabor(
         N_X/3, 3*N_Y/4, **params)*np.exp(-1j*phase)))
@@ -80,7 +71,8 @@ energy /= energy.sum(axis=-1)[:, :, None]
 energy_vector = energy.reshape((N_orient*N_scale, N_X*N_Y))
 energy_plus = np.linalg.pinv(energy_vector)
 
-def accuracy_128(i_offset, j_offset, N_pic=128, N_stim=55):
+
+def accuracy_128(i_offset, j_offset, N_pic=N_X, N_stim=55):
     center = (N_pic-N_stim)//2
 
     accuracy_128 = 0.1 * np.ones((N_pic, N_pic))
@@ -91,9 +83,8 @@ def accuracy_128(i_offset, j_offset, N_pic=128, N_stim=55):
     return accuracy_LP
 
 
-def mnist_128(data, i_offset, j_offset, N_pic=128, N_stim=28, noise=True, noise_type='MotionCloud'):
+def mnist_128(data, i_offset, j_offset, N_pic=N_X, N_stim=28, noise=True, noise_type='MotionCloud'):
     center = (N_pic-N_stim)//2
-    #data_128 = np.zeros((N_pic, N_pic))
     data_128 = (data.min().numpy()) * np.ones((N_pic, N_pic))
 
     data_128[int(center+i_offset):int(center+N_stim+i_offset),
@@ -120,7 +111,7 @@ def couples(data, i_offset, j_offset, device):
 def minmax(value, border):
     value = max(value, -border)
     value = min(value, border)
-    return value
+    return int(value)
 
 
 def sigmoid(values):
@@ -165,7 +156,7 @@ device = torch.device("cuda" if do_cuda else "cpu")
 
 data_loader = torch.utils.data.DataLoader(
     datasets.MNIST('/tmp/data',
-                   train=True,  # def the dataset as training data
+                   train=True,     # def the dataset as training data
                    download=True,  # download if dataset not present on disk
                    transform=transforms.Compose([
                        transforms.ToTensor(),
@@ -175,7 +166,7 @@ data_loader = torch.utils.data.DataLoader(
 
 
 class Net(torch.nn.Module):
-    def __init__(self, n_feature, n_hidden1, n_RNN, n_hidden2, n_output):
+    def __init__(self, n_feature, n_hidden1, n_hidden2, n_output):
         super(Net, self).__init__()
         self.hidden1 = torch.nn.Linear(n_feature, n_hidden1)
         self.hidden2 = torch.nn.Linear(n_hidden1, n_hidden2)
@@ -183,60 +174,16 @@ class Net(torch.nn.Module):
 
     def forward(self, data):
         data = F.leaky_relu(self.hidden1(data))
-        #data, weights = self.RNN(data)
         data = F.leaky_relu(self.hidden2(data))
         data = self.predict(data)
 
         return data
 
 
-if False:
-
-    # cf 2018-05-17_classification_relu_network
-    class Net(torch.nn.Module):
-        def __init__(self, n_feature, n_hidden1, n_hidden2, n_hidden3, n_output):
-            super(Net, self).__init__()
-            self.hidden1 = torch.nn.Linear(n_feature, n_hidden1)
-            self.hidden2 = torch.nn.Linear(n_hidden1, n_hidden2)
-            self.hidden3 = torch.nn.Linear(n_hidden2, n_hidden3)
-            self.predict = torch.nn.Linear(n_hidden3, n_output)
-
-        def forward(self, data):
-            data = F.leaky_relu(self.hidden1(data))
-            data = F.leaky_relu(self.hidden2(data))
-            data = F.leaky_relu(self.hidden3(data))
-            data = self.predict(data)
-            data = F.sigmoid(data)
-            return data
-
-
-    class Net(torch.nn.Module):
-        def __init__(self, n_feature, n_hidden1, n_RNN, n_hidden2, n_output):
-            super(Net, self).__init__()
-            self.hidden1 = torch.nn.Linear(n_feature, n_hidden1)
-            self.RNN = torch.nn.RNN(
-            n_hidden1, n_RNN, nonlinearity='relu', bias=True, bidirectional=False)
-            self.hidden2 = torch.nn.Linear(n_RNN, n_hidden1)
-            self.predict = torch.nn.Linear(n_hidden2, n_output)
-
-        def forward(self, data):
-            data = F.leaky_relu(self.hidden1(data))
-            #data, weights = self.RNN(data)
-            data = F.leaky_relu(self.hidden2(data))
-            data = self.predict(data)
-            data = F.sigmoid(data)
-            return data
-
-
-
-    
-
-net = Net(n_feature=N_theta*N_orient*N_scale*N_phase, n_hidden1=n_hidden1, n_RNN=n_RNN, n_hidden2=n_hidden2, n_output=N_orient*N_scale).to(device)
+net = Net(n_feature=N_theta*N_orient*N_scale*N_phase, n_hidden1=n_hidden1, n_hidden2=n_hidden2, n_output=N_orient*N_scale).to(device)
 optimizer = torch.optim.SGD(net.parameters(), lr=lr)
 loss_func = torch.nn.BCEWithLogitsLoss()
-
-
-    
+   
 def train(net, sample_size, optimizer=optimizer, vsize=N_theta*N_orient*N_scale*N_phase, asize=N_orient*N_scale, offset_std=10, offset_max=25, verbose=1):
     t_start = time.time()
     if verbose: print('Starting training...')
@@ -245,14 +192,14 @@ def train(net, sample_size, optimizer=optimizer, vsize=N_theta*N_orient*N_scale*
             (sample_size, 1, asize))
         target = np.zeros((sample_size, asize))
         for idx in range(sample_size):
-            i_offset, j_offset = int(minmax(
-                np.random.randn()*offset_std, offset_max)), int(minmax(np.random.randn()*offset_std, offset_max))
-            input[idx, 0, :], a_data[idx, 0, :] = couples(
-                data[idx, 0, :], i_offset, j_offset, device)
+            i_offset, j_offset = minmax(np.random.randn()*offset_std, offset_max),  
+                                 minmax(np.random.randn()*offset_std, offset_max)
+            input[idx, 0, :], a_data[idx, 0, :] = couples(data[idx, 0, :], i_offset, j_offset, 
+                                                                                       device)
             target[idx, :] = a_data[idx, 0, :]
 
-        input, target = Variable(torch.FloatTensor(
-            input)), Variable(torch.FloatTensor(a_data))
+        input, target = Variable(torch.FloatTensor(input)), 
+                        Variable(torch.FloatTensor(a_data))
 
         prediction = net(input)
         loss = loss_func(prediction, target)
@@ -274,14 +221,13 @@ def evaluate(net, sample_size, optimizer=optimizer, vsize=N_theta*N_orient*N_sca
             (sample_size, 1, asize))
         target = np.zeros((sample_size, asize))
         for idx in range(sample_size):
-            i_offset, j_offset = int(minmax(
-                np.random.randn()*offset_std, offset_max)), int(minmax(np.random.randn()*offset_std, offset_max))
-            input[idx, 0, :], a_data[idx, 0, :] = couples(
-                data[idx, 0, :], i_offset, j_offset, device)
+            i_offset, j_offset = minmax(np.random.randn()*offset_std, offset_max),
+                                 minmax(np.random.randn()*offset_std, offset_max)
+            input[idx, 0, :], a_data[idx, 0, :] = couples(data[idx, 0, :], i_offset, j_offset, device)
             target[idx, :] = a_data[idx, 0, :]
 
-        input, target = Variable(torch.FloatTensor(
-            input)), Variable(torch.FloatTensor(a_data))
+        input, target = Variable(torch.FloatTensor(input)), 
+                        Variable(torch.FloatTensor(a_data))
 
         prediction = net(input)
         loss = loss_func(prediction, target)
@@ -293,18 +239,17 @@ def evaluate(net, sample_size, optimizer=optimizer, vsize=N_theta*N_orient*N_sca
 def eval_sacc(vsize=N_theta*N_orient*N_scale*N_phase, asize=N_orient*N_scale, N_pic=N_X, sacc_lim=5, fovea_size=10, fig_type='cmap'):
     for batch_idx, (data, label) in enumerate(data_loader):
         data = data.to(device)
-        i_offset, j_offset = int(
-            minmax(np.random.randn()*10, 35)), int(minmax(np.random.randn()*10, 35))
+        i_offset, j_offset = minmax(np.random.randn()*10, 35), 
+                             minmax(np.random.randn()*10, 35)
         print('Stimulus position: ({},{})'.format(i_offset, j_offset))
         a_data_in_fovea = False
         sacc_count = 0
 
         while not a_data_in_fovea:
             input, a_data = np.zeros((1, 1, vsize)), np.zeros((1, 1, asize))
-            input[0, 0, :], a_data[0, 0, :] = couples(
-                data[0, 0, :], i_offset, j_offset, device)
-            input, a_data = Variable(torch.FloatTensor(
-                input)), Variable(torch.FloatTensor(a_data))
+            input[0, 0, :], a_data[0, 0, :] = couples(data[0, 0, :], i_offset, j_offset, device)
+            input, a_data = Variable(torch.FloatTensor(input)), 
+                            Variable(torch.FloatTensor(a_data))
 
             prediction = net(input)
             pred_data = prediction.data.numpy()[-1][-1]
@@ -346,14 +291,11 @@ def eval_sacc(vsize=N_theta*N_orient*N_scale*N_phase, asize=N_orient*N_scale, N_
                 if i_hat <= (fovea_size/2) and j_hat <= (fovea_size/2):
                     if i_hat >= -(fovea_size/2) and j_hat >= -(fovea_size/2):
                         a_data_in_fovea = True
-                        print(
-                            'a_data predicted in fovea, stopping the saccadic exploration')
+                        print('a_data predicted in fovea, stopping the saccadic exploration')
 
             if fig_type == 'log':
-                code = energy_plus @ pred_data #np.ravel(pred_data)
+                code = energy_plus @ pred_data
                 global_energy = energy_vector @ code
-                #code = phi @ code
-                #global_energy = (code**2).sum(axis=(0, -1))
                 global_energy = global_energy.reshape(N_scale, N_orient)
 
 
@@ -364,10 +306,9 @@ def eval_sacc(vsize=N_theta*N_orient*N_scale*N_phase, asize=N_orient*N_scale, N_
                     theta_a_data = np.arctan(-i_offset / j_offset)
                 else:
                     theta_a_data = np.sign(-i_offset) * np.pi/2
-                print('a_data position (log_r, theta) = ({},{})'.format(
-                    log_r_a_data, theta_a_data))
-                log_r, theta = np.meshgrid(np.linspace(
-                    0, 1, N_scale+1), np.linspace(-np.pi*.625, np.pi*1.375, N_orient+1))
+                print('a_data position (log_r, theta) = ({},{})'.format(log_r_a_data, 
+                                                                        theta_a_data))
+                log_r, theta = np.meshgrid(np.linspace(0, 1, N_scale+1), np.linspace(-np.pi*.625, np.pi*1.375, N_orient+1))
 
                 fig, ax = plt.subplots(subplot_kw=dict(projection='polar'))
                 ax.pcolor(theta, log_r, np.fliplr(global_energy))
