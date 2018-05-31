ffe6600dee5d60ecddbca5f631c84d301df6e5b6
Laurent Perrinet <laurent.perrinet@gmail.com>
2018-05-31 11:59:29 +0200

    simplifying code : azimuth / eccentricity

--- Notebooks/Where.py
+++ Notebooks/Where.py
@@ -6,12 +6,12 @@ import argparse
 import noise
 
 # TODO: passer les arguments par la ligne de commande
-N_theta, N_orient, N_scale, N_phase, N_X, N_Y, rho = 6, 12, 5, 2, 128, 128, 1.61803
+N_theta, N_azimuth, N_eccentricty, N_phase, N_X, N_Y, rho = 6, 12, 15, 2, 128, 128, 1.61803
 sample_size = 100  # quantity of examples that'll be processed
 lr = 0.05
-n_hidden1 = int(((N_theta*N_orient*N_scale*N_phase)/4)*3)
-n_hidden2 = int(((N_theta*N_orient*N_scale*N_phase)/4))
-
+n_hidden1 = int(((N_theta*N_azimuth*N_eccentricty*N_phase)/4)*3)
+n_hidden2 = int(((N_theta*N_azimuth*N_eccentricty*N_phase)/4))
+verbose = 1
 
 
 import torch
@@ -27,14 +27,15 @@ from LogGabor import LogGabor
 ## Charger la matrice de certitude
 path = "MNIST_accuracy.npy"
 if os.path.isfile(path):
-    print('Loading accuracy...')
     accuracy =  np.load(path)
+    if verbose:
+        print('Loading accuracy... min, max=', accuracy.min(), accuracy.max())
 else:
     print('No accuracy data found.')
 
 ## Préparer l'apprentissage et les fonctions nécessaires au fonctionnement du script
-def vectorization(N_theta, N_orient, N_scale, N_phase, N_X, N_Y, rho):
-    retina = np.zeros((N_theta, N_orient, N_scale, N_phase, N_X*N_Y))
+def vectorization(N_theta, N_azimuth, N_eccentricty, N_phase, N_X, N_Y, rho):
+    retina = np.zeros((N_theta, N_azimuth, N_eccentricty, N_phase, N_X*N_Y))
     parameterfile = 'https://raw.githubusercontent.com/bicv/LogGabor/master/default_param.py'
     lg = LogGabor(parameterfile)
     lg.set_size((N_X, N_Y))
@@ -45,50 +46,35 @@ def vectorization(N_theta, N_orient, N_scale, N_phase, N_X, N_Y, rho):
         N_X/3, 3*N_Y/4, **params)*np.exp(-1j*phase)))
 
     for i_theta in range(N_theta):
-        for i_orient in range(N_orient):
-            for i_scale in range(N_scale):
-                ecc = (1/rho)**(N_scale - i_scale)
+        for i_azimuth in range(N_azimuth):
+            for i_eccentricty in range(N_eccentricty):
+                ecc = (1/rho)**(N_eccentricty - i_eccentricty)
                 r = np.sqrt(N_X**2+N_Y**2) / 2 * ecc  # radius
                 sf_0 = 0.5 * 0.03 / ecc
                 x = N_X/2 + r * \
-                    np.cos((i_orient+(i_scale % 2)*.5)*np.pi*2 / N_orient)
+                    np.cos((i_azimuth+(i_eccentricty % 2)*.5)*np.pi*2 / N_azimuth)
                 y = N_Y/2 + r * \
-                    np.sin((i_orient+(i_scale % 2)*.5)*np.pi*2 / N_orient)
+                    np.sin((i_azimuth+(i_eccentricty % 2)*.5)*np.pi*2 / N_azimuth)
                 for i_phase in range(N_phase):
                     params = {'sf_0': sf_0, 'B_sf': lg.pe.B_sf,
                               'theta': i_theta*np.pi/N_theta, 'B_theta': np.pi/N_theta/2}
                     phase = i_phase * np.pi/2
-                    retina[i_theta, i_orient, i_scale, i_phase, :] = lg.normalize(
+                    retina[i_theta, i_azimuth, i_eccentricty, i_phase, :] = lg.normalize(
                         lg.invert(lg.loggabor(x, y, **params)*np.exp(-1j*phase))).ravel()
     return retina
 
 
-retina = vectorization(N_theta, N_orient, N_scale, N_phase, N_X, N_Y, rho)
-retina_vector = retina.reshape((N_theta*N_orient*N_scale*N_phase, N_X*N_Y))
+retina = vectorization(N_theta, N_azimuth, N_eccentricty, N_phase, N_X, N_Y, rho)
+retina_vector = retina.reshape((N_theta*N_azimuth*N_eccentricty*N_phase, N_X*N_Y))
 retina_inverse = np.linalg.pinv(retina_vector)
 
 colliculus = (retina**2).sum(axis=(0, 3))
 colliculus /= colliculus.sum(axis=-1)[:, :, None]
-colliculus_vector = colliculus.reshape((N_orient*N_scale, N_X*N_Y))
+colliculus_vector = colliculus.reshape((N_azimuth*N_eccentricty, N_X*N_Y))
 colliculus_inverse = np.linalg.pinv(colliculus_vector)
 
 
-def accuracy_128(accuracy, i_offset, j_offset, N_pic=N_X):
-    N_stim = accuracy.shape[0]
-    center = (N_pic-N_stim)//2
-
-    accuracy_128 = 0.1 * np.ones((N_pic, N_pic))
-
-    accuracy_128[(center+i_offset):(center+N_stim+i_offset), (center+j_offset):(center+N_stim+j_offset)] = accuracy
-    accuracy_128[int(center+i_offset):int(center+N_stim+i_offset),
-                 int(center+j_offset):int(center+N_stim+j_offset)] = accuracy
-
-
-    accuracy_LP = colliculus_vector @ np.ravel(accuracy_128)
-    return accuracy_LP
-
-
-def mnist_128(data, i_offset, j_offset, N_pic=N_X, noise=0.):
+def mnist_fullfield(data, i_offset, j_offset, N_pic=N_X, noise=0.):
     N_stim = data.shape[0]
     center = (N_pic-N_stim)//2
 
@@ -98,15 +84,30 @@ def mnist_128(data, i_offset, j_offset, N_pic=N_X, noise=0.):
     if noise>0.:
         data_128 += noise * MotionCloudNoise()
 
-    data_LP = retina_vector @ np.ravel(data_128)
+    data_retina = retina_vector @ np.ravel(data_128)
 
-    return data_LP
+    return data_retina
+
+
+def accuracy_fullfield(accuracy, i_offset, j_offset, N_pic=N_X):
+    N_stim = accuracy.shape[0]
+    center = (N_pic-N_stim)//2
+
+    accuracy_fullfield = 0.1 * np.ones((N_pic, N_pic))
+    accuracy_fullfield[int(center+i_offset):int(center+N_stim+i_offset),
+                 int(center+j_offset):int(center+N_stim+j_offset)] = accuracy
+
+
+    accuracy_colliculus = colliculus_vector @ np.ravel(accuracy_fullfield)
+    # if verbose: print('accuracy_colliculus... min, max=', accuracy_colliculus.min(), accuracy_colliculus.max())
+
+    return accuracy_colliculus
 
 
 def couples(data, i_offset, j_offset):#, device):
     #data = data.to(device)
-    v = mnist_128(data, i_offset, j_offset)
-    a = accuracy_128(accuracy, i_offset, j_offset)
+    v = mnist_fullfield(data, i_offset, j_offset)
+    a = accuracy_fullfield(accuracy, i_offset, j_offset)
     return (v, a)
 
 
@@ -154,26 +155,29 @@ class Net(torch.nn.Module):
         data = F.leaky_relu(self.hidden1(data))
         data = F.leaky_relu(self.hidden2(data))
         data = self.predict(data)
-
         return data
 
 #print(device)
-net = Net(n_feature=N_theta*N_orient*N_scale*N_phase, n_hidden1=n_hidden1, n_hidden2=n_hidden2, n_output=N_orient*N_scale)
+net = Net(n_feature=N_theta*N_azimuth*N_eccentricty*N_phase, n_hidden1=n_hidden1, n_hidden2=n_hidden2, n_output=N_azimuth*N_eccentricty)
 #net = net.to(device)
 optimizer = torch.optim.SGD(net.parameters(), lr=lr)
+# https://pytorch.org/docs/master/nn.html?highlight=bcewithlogitsloss#torch.nn.BCEWithLogitsLoss
 loss_func = torch.nn.BCEWithLogitsLoss()
 
 
-def train(net, sample_size, optimizer=optimizer, vsize=N_theta*N_orient*N_scale*N_phase, asize=N_orient*N_scale, offset_std=10, offset_max=25, verbose=1):
+def train(net, sample_size, optimizer=optimizer, vsize=N_theta*N_azimuth*N_eccentricty*N_phase, asize=N_azimuth*N_eccentricty, offset_std=10, offset_max=25, verbose=1):
     t_start = time.time()
     if verbose: print('Starting training...')
     for batch_idx, (data, label) in enumerate(data_loader):
-        input, a_data = np.zeros((sample_size, 1, vsize)), np.zeros(
-            (sample_size, 1, asize))
+
+        input = np.zeros((sample_size, 1, vsize))
+        a_data = np.zeros((sample_size, 1, asize))
+
         target = np.zeros((sample_size, asize))
         for idx in range(sample_size):
 
-            i_offset, j_offset = minmax(np.random.randn()*offset_std, offset_max), minmax(np.random.randn()*offset_std, offset_max)
+            i_offset = minmax(np.random.randn()*offset_std, offset_max)
+            j_offset = minmax(np.random.randn()*offset_std, offset_max)
             input[idx, 0, :], a_data[idx, 0, :] = couples(data[idx, 0, :], i_offset, j_offset)
             target[idx, :] = a_data[idx, 0, :]
 
@@ -193,7 +197,7 @@ def train(net, sample_size, optimizer=optimizer, vsize=N_theta*N_orient*N_scale*
     return net
 
 
-def test(net, sample_size, optimizer=optimizer, vsize=N_theta*N_orient*N_scale*N_phase, asize=N_orient*N_scale, offset_std=10, offset_max=25):
+def test(net, sample_size, optimizer=optimizer, vsize=N_theta*N_azimuth*N_eccentricty*N_phase, asize=N_azimuth*N_eccentricty, offset_std=10, offset_max=25):
     for batch_idx, (data, label) in enumerate(data_loader):
         input, a_data = np.zeros((sample_size, 1, vsize)), np.zeros(
             (sample_size, 1, asize))
@@ -213,20 +217,20 @@ def test(net, sample_size, optimizer=optimizer, vsize=N_theta*N_orient*N_scale*N
     return loss.data.numpy()
 
 
-def eval_sacc(vsize=N_theta*N_orient*N_scale*N_phase, asize=N_orient*N_scale, N_pic=N_X, sacc_lim=5, fovea_size=10, offset_std=10, offset_max=25, fig_type='cmap'):
+def eval_sacc(vsize=N_theta*N_azimuth*N_eccentricty*N_phase, asize=N_azimuth*N_eccentricty,
+             N_pic=N_X, sacc_lim=5, fovea_size=10, offset_std=10, offset_max=25, fig_type='cmap'):
     for batch_idx, (data, label) in enumerate(data_loader):
         #data = data.to(device)
         i_offset = minmax(np.random.randn()*offset_std, offset_max)
         j_offset = minmax(np.random.randn()*offset_std, offset_max)
         print('Stimulus position: ({},{})'.format(i_offset, j_offset))
-        a_data_in_fovea = False
-        sacc_count = 0
+        # a_data_in_fovea = False
+        # sacc_count = 0
 
-        while not a_data_in_fovea:
+        if True: #while not a_data_in_fovea:
             input, a_data = np.zeros((1, 1, vsize)), np.zeros((1, 1, asize))
             input[0, 0, :], a_data[0, 0, :] = couples(data[0, 0, :], i_offset, j_offset)
-            input, a_data = Variable(torch.FloatTensor(input)), Variable(torch.FloatTensor(a_data))
-            input, a_data = Variable(torch.FloatTensor(input)), Variable(torch.FloatTensor(a_data))
+            #input, a_data = Variable(torch.FloatTensor(input)), Variable(torch.FloatTensor(a_data))
 
             input = Variable(torch.FloatTensor(input))
             a_data = Variable(torch.FloatTensor(a_data))
@@ -256,26 +260,29 @@ def eval_sacc(vsize=N_theta*N_orient*N_scale*N_phase, asize=N_orient*N_scale, N_
                                 plt.axhline(i_hat, c='r')
                             break
 
-                # check if number of saccades is beyond threshold
-                if sacc_count == sacc_lim:
-                    print('Stimulus position not found, break')
-                    break
-
-                # saccades
-                i_offset, j_offset = (i_offset - i_hat), (j_offset - j_hat)
-                sacc_count += 1
-                print('Stimulus position after saccade: ({}, {})'.format(i_offset, j_offset))
-
-                # check if the image position is predicted within the fovea
-                if i_hat <= (fovea_size/2) and j_hat <= (fovea_size/2):
-                    if i_hat >= -(fovea_size/2) and j_hat >= -(fovea_size/2):
-                        a_data_in_fovea = True
-                        print('a_data predicted in fovea, stopping the saccadic exploration')
+                # # check if number of saccades is beyond threshold
+                # if sacc_count == sacc_lim:
+                #     print('Stimulus position not found, break')
+                #     break
+                #
+                # # saccades
+                # i_offset, j_offset = (i_offset - i_hat), (j_offset - j_hat)
+                # sacc_count += 1
+                # print('Stimulus position after saccade: ({}, {})'.format(i_offset, j_offset))
+                #
+                # # check if the image position is predicted within the fovea
+                # if i_hat <= (fovea_size/2) and j_hat <= (fovea_size/2):
+                #     if i_hat >= -(fovea_size/2) and j_hat >= -(fovea_size/2):
+                #         a_data_in_fovea = True
+                #         print('a_data predicted in fovea, stopping the saccadic exploration')
 
             if fig_type == 'log':
-                code = colliculus_inverse @ pred_data
-                global_colliculus = colliculus_vector @ code
-                global_colliculus = global_colliculus.reshape(N_scale, N_orient)
+                print(pred_data.shape)
+                print('Loading pred_data... min, max=', pred_data.min(), pred_data.max())
+
+                # code = colliculus_inverse @ pred_data
+                # global_colliculus = colliculus_vector @ code
+                global_colliculus = pred_data.reshape(N_eccentricty, N_azimuth)
 
                 log_r_a_data = 1 + \
                     np.log(np.sqrt(i_offset**2 + j_offset**2) /
@@ -286,17 +293,17 @@ def eval_sacc(vsize=N_theta*N_orient*N_scale*N_phase, asize=N_orient*N_scale, N_
                     theta_a_data = np.sign(-i_offset) * np.pi/2
                 print('a_data position (log_r, theta) = ({},{})'.format(log_r_a_data,
                                                                         theta_a_data))
-                log_r, theta = np.meshgrid(np.linspace(0, 1, N_scale+1), np.linspace(-np.pi*.625, np.pi*1.375, N_orient+1))
+                log_r, theta = np.meshgrid(np.linspace(0, 1, N_eccentricty+1), np.linspace(-np.pi*.625, np.pi*1.375, N_azimuth+1))
 
                 fig, ax = plt.subplots(subplot_kw=dict(projection='polar'))
                 ax.pcolor(theta, log_r, np.fliplr(global_colliculus))
                 ax.plot(theta_a_data, log_r_a_data, 'r+')
-
-                for n_orient in range(N_orient):
-                    for n_scale in range(N_scale):
-                        if global_colliculus[n_orient][n_scale] == np.max(global_colliculus):
-                            print('Position prediction (orient, scale) = ({},{})'.format(
-                                n_orient, n_scale))
+                #
+                # for i_azimuth in range(N_azimuth):
+                #     for i_eccentricty in range(N_eccentricty):
+                #         if global_colliculus[i_azimuth][i_eccentricty] == np.max(global_colliculus):
+                #             print('Position prediction (orient, scale) = ({},{})'.format(
+                #                 i_azimuth, i_eccentricty))
 
                 a_data_in_fovea = True
 
